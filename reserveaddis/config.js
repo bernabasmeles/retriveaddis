7e1a65655c4747a7a6f80936210ea58b5929a80d
Bernabas Meles <109125075+bernabasmeles@users.noreply.github.com>
2023-05-04 20:19:21 +0300

    Add files via upload

---
+++ reserveaddis/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "qwertyuiopasdfghjklzxcvbnm123456",
+    EMAIL: "bernabasmeles123@gmail.com", 
+    PASSWORD : "",
+    ATLAS_URI: "mongodb+srv://user1:pass1@cluster0.cs34tsw.mongodb.net/?retryWrites=true&w=majority"
+}
